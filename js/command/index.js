31f2e4ba2bb8557b6955442c7e7f9e47f3d32d26
luxianfang <30764630+luxianfang@users.noreply.github.com>
2020-02-23 17:42:10 +0800

    Add files via upload

--- js/command/index.js
+++ js/command/index.js
@@ -52,48 +52,36 @@ var vue = new Vue({
 		preView: function() {
 			console.log($('#design'));
 			console.log($('#design')[0].innerHTML);
-			localStorage.setItem("html",$('#design')[0].innerHTML);
-			// getChildren($('#design').children());
-			// console.log(arr);
+			localStorage.setItem("html", $('#design')[0].innerHTML);
 			window.open("preview.html");
+		},
+		drag: function(ev, index) {
+			ev.dataTransfer.setData("id", ev.target.id);
+			ev.dataTransfer.setData("html", this.$data.controlList[index].html);
+		},
+		drop: function(ev) {
+			ev.preventDefault();
+			var html = ev.dataTransfer.getData("html");
+			$(ev.target).on("click", elClick);
+			$(ev.target).append(html);
+		},
+		allowDrop: function(ev) {
+			ev.preventDefault();
+		},
+		nameChange: function(ev) {
+			if (currEl != undefined) {
+				console.log(ev.target.value)
+				console.log(currEl)
+				currEl.setAttribute("name", ev.target.value)
+				console.log(currEl)
+			}
+
+		},
+		valueChange: function() {
+
 		}
 	}
 });
-
-function allowDrop(ev) {
-	ev.preventDefault();
-}
-
-function drag(ev) {
-	ev.dataTransfer.setData("Text", ev.target.id);
-}
-
-function drop(ev) {
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData("Text");
-	// ev.target.appendChild(document.getElementById(data));
-	// $(ev.target.id).Tdrag({
-	// 	scope: ".design"
-	// })
-
-	var html = "";
-	if (data == 'div') {
-		html = "<div style='width:100%;min-height: 100px;' class='control'></div>"
-	}
-	if (data == 'label') {
-		html = "<label class='control'>文本</label>"
-	}
-	if (data == 'input') {
-		html = "<input class='control' v-model='name'></input>"
-	}
-	if (data == 'label_input') {
-		html = "<div style='width:100%;min-height: 23px;' class='control'><label>文本</label><input></input></div>"
-	}
-
-	$(ev.target).on("click", elClick);
-	$(ev.target).append(html);
-	console.log($('#design').children());
-}
 
 var arr = [];
 
@@ -106,18 +94,38 @@ function getChildren(el) {
 	})
 }
 
-// localStorage.clear();
+function setName(el) {
+	var r;
+	$.each(el, function(index, obj) {
+		if (obj.children) {
+			setName(obj.children);
+		}
+		obj.setAttribute("id", obj.localName + "_" + guid())
+		obj.setAttribute("name", obj.localName + "_" + guid())
+		r = obj;
+	})
+	return r;
+}
+
 var currEl;
 
+
 function elClick(e) {
-	console.log(e.target.id)
+	console.log(e.target)
 	if (e.target.id == 'design') return;
 	if (currEl != null) {
 		currEl.setAttribute("class", "control")
 	}
 	currEl = e.target;
-	// console.log(this)
 	e.target.setAttribute("class", "controlSelected")
+	if (!e.target.id) {
+		e.target.setAttribute("id", e.target.localName + "_" + guid())
+	}
+	if (!e.target.name) {
+		e.target.setAttribute("name", e.target.localName + "_" + guid())
+	}
+	$("#name").val(e.target.name)
+	$("#value").val(e.target.value)
 }
 
 function guid() {
